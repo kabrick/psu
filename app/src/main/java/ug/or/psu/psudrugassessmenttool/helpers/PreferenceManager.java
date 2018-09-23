163d8d38ef2f6565df201994c3af18a999fab811
kabricks <kabuyedouglas53@gmail.com>
2018-09-23 16:10:45 +0300

    initalize preference manager class with constructor and variable names

--- app/src/main/java/ug/or/psu/psudrugassessmenttool/helpers/PreferenceManager.java
+++ app/src/main/java/ug/or/psu/psudrugassessmenttool/helpers/PreferenceManager.java
@@ -1,4 +1,27 @@
 package ug.or.psu.psudrugassessmenttool.helpers;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class PreferenceManager {
+
+    private static final String PREF_NAME = "psu_preferences";
+
+    private SharedPreferences.Editor editor;
+    private SharedPreferences pref;
+
+    /**
+     * Class constructor to set privacy mode and create new editor
+     *
+     * @param context application context
+     *
+     * suppress for adding commit to created prefs editor
+     */
+    @SuppressLint("CommitPrefEdits")
+    public PreferenceManager(Context context) {
+        int PRIVATE_MODE = 0;
+        this.pref = context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        this.editor = this.pref.edit();
+    }
 }
