61b33f48305fbb01fb1af2d0dd79945257898af9
kabricks <kabuyedouglas53@gmail.com>
2018-09-28 09:23:00 +0300

    add helper functions for alert dialog and snackbar displays

--- app/src/main/java/ug/or/psu/psudrugassessmenttool/helpers/HelperFunctions.java
+++ app/src/main/java/ug/or/psu/psudrugassessmenttool/helpers/HelperFunctions.java
@@ -1,4 +1,46 @@
 package ug.or.psu.psudrugassessmenttool.helpers;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+
 public class HelperFunctions {
+
+    private Context context;
+    private PreferenceManager prefManager;
+
+    public HelperFunctions(Context context) {
+        this.context = context;
+        prefManager = new PreferenceManager(this.context);
+    }
+
+    /**
+     * display alert dialog anywhere in app
+     *
+     * @param message text to display
+     */
+    public void genericDialog(String message) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(context);
+
+        alert.setMessage(message).setPositiveButton("Okay", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                //
+            }
+        }).show();
+    }
+
+    /**
+     * generic snackbar passing the message and view
+     *
+     * @param message text to display
+     * @param view view to display on
+     */
+    public void genericSnackbar(String message, View view){
+        Snackbar mySnackbar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT);
+        mySnackbar.show();
+    }
+
 }
