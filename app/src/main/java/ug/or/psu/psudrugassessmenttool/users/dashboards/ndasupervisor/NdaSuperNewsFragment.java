365fc9dada032bbff311120e841276c0d9626410
kabricks <kabuyedouglas53@gmail.com>
2018-10-01 16:33:37 +0300

    reverse changes made for the news feed

--- app/src/main/java/ug/or/psu/psudrugassessmenttool/users/dashboards/ndasupervisor/NdaSuperNewsFragment.java
+++ app/src/main/java/ug/or/psu/psudrugassessmenttool/users/dashboards/ndasupervisor/NdaSuperNewsFragment.java
@@ -6,40 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
-import com.android.volley.Cache;
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonObjectRequest;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
-
-import ug.or.psu.psudrugassessmenttool.MainActivity;
 import ug.or.psu.psudrugassessmenttool.R;
-import ug.or.psu.psudrugassessmenttool.adapters.NewsFeedAdapter;
-import ug.or.psu.psudrugassessmenttool.models.NewsFeed;
-import ug.or.psu.psudrugassessmenttool.network.VolleySingleton;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NdaSuperNewsFragment extends Fragment {
 
-    private static final String TAG = NdaSuperNewsFragment.class.getSimpleName();
-    private ListView listView;
-    private NewsFeedAdapter listAdapter;
-    private List<NewsFeed> feedItems;
-    private String URL_FEED = "https://api.androidhive.info/feed/feed.json";
-    View view;
 
     public NdaSuperNewsFragment() {
         // Required empty public constructor
@@ -50,91 +24,7 @@ public class NdaSuperNewsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_nda_super_news, container, false);
-
-        listView = (ListView)view.findViewById(R.id.list);
-
-        feedItems = new ArrayList<NewsFeed>();
-
-        listAdapter = new NewsFeedAdapter(this, feedItems, getContext());
-        listView.setAdapter(listAdapter);
-
-        // We first check for cached request
-        Cache cache = VolleySingleton.getInstance(getContext()).getRequestQueue().getCache();
-        Cache.Entry entry = cache.get(URL_FEED);
-        if (entry != null) {
-            // fetch the data from cache
-            try {
-                String data = new String(entry.data, "UTF-8");
-                try {
-                    parseJsonFeed(new JSONObject(data));
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-
-        } else {
-            // making fresh volley request and getting json
-            JsonObjectRequest jsonReq = new JsonObjectRequest(Request.Method.GET,
-                    URL_FEED, null, new Response.Listener<JSONObject>() {
-
-                @Override
-                public void onResponse(JSONObject response) {
-                    VolleyLog.d(TAG, "Response: " + response.toString());
-                    parseJsonFeed(response);
-                }
-            }, new Response.ErrorListener() {
-
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    VolleyLog.d(TAG, "Error: " + error.getMessage());
-                }
-            });
-
-            // Adding request to volley request queue
-            VolleySingleton.getInstance(getContext()).addToRequestQueue(jsonReq);
-        }
-
-        return view;
-    }
-
-    /**
-     * Parsing json reponse and passing the data to feed view list adapter
-     * */
-    private void parseJsonFeed(JSONObject response) {
-        try {
-            JSONArray feedArray = response.getJSONArray("feed");
-
-            for (int i = 0; i < feedArray.length(); i++) {
-                JSONObject feedObj = (JSONObject) feedArray.get(i);
-
-                NewsFeed item = new NewsFeed();
-                item.setId(feedObj.getInt("id"));
-                item.setName(feedObj.getString("name"));
-
-                // Image might be null sometimes
-                String image = feedObj.isNull("image") ? null : feedObj
-                        .getString("image");
-                item.setImge(image);
-                item.setStatus(feedObj.getString("status"));
-                item.setProfilePic(feedObj.getString("profilePic"));
-                item.setTimeStamp(feedObj.getString("timeStamp"));
-
-                // url might be null sometimes
-                String feedUrl = feedObj.isNull("url") ? null : feedObj
-                        .getString("url");
-                item.setUrl(feedUrl);
-
-                feedItems.add(item);
-            }
-
-            // notify data changes to list adapter
-            listAdapter.notifyDataSetChanged();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+        return inflater.inflate(R.layout.fragment_nda_super_news, container, false);
     }
 
 }
