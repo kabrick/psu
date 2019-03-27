8dbde216388b68235923d42dc2537a8fd549d547
kabricks <kabuyedouglas53@gmail.com>
2019-03-27 15:55:45 +0300

    place image on the same page as profie details for easier access

--- app/src/main/java/ug/or/psu/psudrugassessmenttool/globalactivities/EditProfileActivity.java
+++ app/src/main/java/ug/or/psu/psudrugassessmenttool/globalactivities/EditProfileActivity.java
@@ -1,31 +1,39 @@
 package ug.or.psu.psudrugassessmenttool.globalactivities;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.NetworkError;
-import com.android.volley.NoConnectionError;
-import com.android.volley.ParseError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
 import com.android.volley.Request;
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
-import com.android.volley.ServerError;
-import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import ug.or.psu.psudrugassessmenttool.R;
 import ug.or.psu.psudrugassessmenttool.helpers.HelperFunctions;
 import ug.or.psu.psudrugassessmenttool.helpers.PreferenceManager;
+import ug.or.psu.psudrugassessmenttool.network.VolleyMultipartRequest;
 import ug.or.psu.psudrugassessmenttool.network.VolleySingleton;
 
 public class EditProfileActivity extends AppCompatActivity {
@@ -33,6 +41,9 @@ public class EditProfileActivity extends AppCompatActivity {
     PreferenceManager preferenceManager;
     HelperFunctions helperFunctions;
     EditText name, username, email, phone;
+    ImageView profile_picture;
+    private final int IMAGE_REQUEST_CODE = 1;
+    private RequestQueue rQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,12 +63,15 @@ public class EditProfileActivity extends AppCompatActivity {
         email = findViewById(R.id.email);
         phone = findViewById(R.id.phone);
 
+        // image
+        profile_picture = findViewById(R.id.profile_picture);
+
         getProfileDetails();
     }
 
     public void getProfileDetails(){
 
-        helperFunctions.genericProgressBar("Fetching records...");
+        helperFunctions.genericProgressBar("Fetching profile details...");
 
         String network_address = helperFunctions.getIpAddress()
                 + "get_profile_details.php?id=" + preferenceManager.getPsuId();
@@ -67,7 +81,7 @@ public class EditProfileActivity extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
 
-                        helperFunctions.stopProgressBar();
+                        getProfilePicture();
 
                         try {
                             name.setText(response.getString("name"));
@@ -86,6 +100,40 @@ public class EditProfileActivity extends AppCompatActivity {
         });
 
         VolleySingleton.getInstance(EditProfileActivity.this).addToRequestQueue(request);
+    }
+
+    public void getProfilePicture(){
+
+        String network_address = helperFunctions.getIpAddress()
+                + "get_profile_picture.php?id=" + preferenceManager.getPsuId();
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, network_address, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        try {
+
+                            helperFunctions.stopProgressBar();
+
+                            String picture = helperFunctions.getIpAddress() + response.getString("photo");
+                            Glide.with(EditProfileActivity.this)
+                                    .load(picture)
+                                    .into(profile_picture);
+
+                            //.apply(RequestOptions.circleCropTransform())
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //
+            }
+        });
+
+        VolleySingleton.getInstance(this).addToRequestQueue(request);
     }
 
     public void saveProfile(View view){
@@ -125,9 +173,89 @@ public class EditProfileActivity extends AppCompatActivity {
         VolleySingleton.getInstance(this).addToRequestQueue(request);
     }
 
-    public void editProfilePicture(View view){
-        Intent intent_profile = new Intent(EditProfileActivity.this, EditProfilePictureActivity.class);
-        startActivity(intent_profile);
+    public void changeProfilePicture(View view){
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(galleryIntent, IMAGE_REQUEST_CODE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+
+        // check if user has selected an image
+        if (requestCode == IMAGE_REQUEST_CODE) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+                    profile_picture.setImageBitmap(bitmap);
+                    helperFunctions.genericProgressBar("Updating your profile picture");
+                    uploadProfilePicture(bitmap);
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void uploadProfilePicture(final Bitmap bitmap){
+        String upload_URL = helperFunctions.getIpAddress() + "update_user_profile_picture.php";
+
+        VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, upload_URL,
+                new Response.Listener<NetworkResponse>() {
+                    @Override
+                    public void onResponse(NetworkResponse response) {
+                        rQueue.getCache().clear();
+                        helperFunctions.stopProgressBar();
+                        helperFunctions.genericDialog("Profile picture updated successfully");
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams(){
+                Map<String, String> params = new HashMap<>();
+                // add the psu_id
+                params.put("psu_id", preferenceManager.getPsuId());
+                return params;
+            }
+
+            /*
+             *pass files using below method
+             * */
+            @Override
+            protected Map<String, DataPart> getByteData() {
+                Map<String, DataPart> params = new HashMap<>();
+                long imagename = System.currentTimeMillis();
+                params.put("filename", new DataPart(imagename + ".png", getFileDataFromDrawable(bitmap)));
+                return params;
+            }
+        };
+
+        volleyMultipartRequest.setRetryPolicy(new DefaultRetryPolicy(
+                0,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        rQueue = Volley.newRequestQueue(this);
+        rQueue.add(volleyMultipartRequest);
+    }
+
+    public byte[] getFileDataFromDrawable(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
     }
 
     @Override
