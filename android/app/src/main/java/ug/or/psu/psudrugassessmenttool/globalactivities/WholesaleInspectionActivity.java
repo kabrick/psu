cc1dae61f9bb90e622455c36666379213361de06
kabrick <kabuyedouglas53@gmail.com>
2019-06-05 16:44:40 +0300

    complete the wholesale inspection

--- android/app/src/main/java/ug/or/psu/psudrugassessmenttool/globalactivities/WholesaleInspectionActivity.java
+++ android/app/src/main/java/ug/or/psu/psudrugassessmenttool/globalactivities/WholesaleInspectionActivity.java
@@ -38,18 +38,26 @@ public class WholesaleInspectionActivity extends AppCompatActivity {
             supervising_pharmacist, reg_number, support_supervision_date, location;
     TextView section_a_total_score, section_a_percentage, section_b_total_score, section_b_percentage,
             overall_total_score, overall_percentage_score, pharmacy_category, main_title,
-            section_c_total_score, section_c_percentage, section_d_total_score, section_d_percentage;
+            section_c_total_score, section_c_percentage, section_d_total_score, section_d_percentage, section_e_total_score, section_e_percentage, section_f_total_score, section_f_percentage;
     RadioGroup section_a_1, section_a_2, section_a_3, section_a_4, section_a_5,
             section_b_1, section_b_2, section_b_3, section_b_4, section_b_5,
             section_c_1, section_c_2, section_c_3, section_c_4, section_c_5,
             section_c_6, section_c_7, section_c_8, section_c_9, section_c_10,
             section_d_1, section_d_2, section_d_3, section_d_4, section_d_5,
-            section_d_6, section_d_7, section_d_8, section_d_9, section_d_10, section_d_11;
+            section_d_6, section_d_7, section_d_8, section_d_9, section_d_10, section_d_11,
+            section_e_1, section_e_2, section_e_3, section_e_4, section_e_5,
+            section_e_6, section_e_7, section_e_8, section_e_9, section_e_10, section_e_11, section_e_12,
+            section_f_1, section_f_2, section_f_3, section_f_4, section_f_5,
+            section_f_6, section_f_7;
     int section_a_1_int, section_a_2_int, section_a_3_int, section_a_4_int, section_a_5_int,
             section_c_1_int, section_c_2_int, section_c_3_int, section_c_4_int, section_c_5_int,
             section_c_6_int, section_c_7_int, section_c_8_int, section_c_9_int, section_c_10_int,
             section_d_1_int, section_d_2_int, section_d_3_int, section_d_4_int, section_d_5_int,
             section_d_6_int, section_d_7_int, section_d_8_int, section_d_9_int, section_d_10_int, section_d_11_int,
+            section_e_1_int, section_e_2_int, section_e_3_int, section_e_4_int, section_e_5_int,
+            section_e_6_int, section_e_7_int, section_e_8_int, section_e_9_int, section_e_10_int, section_e_11_int, section_e_12_int,
+            section_f_1_int, section_f_2_int, section_f_3_int, section_f_4_int, section_f_5_int,
+            section_f_6_int, section_f_7_int,
             section_b_1_int, section_b_2_int, section_b_3_int, section_b_4_int, section_b_5_int, section_a_total, section_b_total, section_c_total, section_d_total, section_e_total, section_f_total = 0;
 
     @SuppressLint("SetTextI18n")
@@ -88,6 +96,10 @@ public class WholesaleInspectionActivity extends AppCompatActivity {
         section_c_percentage = findViewById(R.id.section_c_percentage);
         section_d_total_score = findViewById(R.id.section_d_total_score);
         section_d_percentage = findViewById(R.id.section_d_percentage);
+        section_e_total_score = findViewById(R.id.section_e_total_score);
+        section_e_percentage = findViewById(R.id.section_e_percentage);
+        section_f_total_score = findViewById(R.id.section_f_total_score);
+        section_f_percentage = findViewById(R.id.section_f_percentage);
         additional_notes = findViewById(R.id.additional_notes);
         pharmacy_name = findViewById(R.id.pharmacy_name);
         contact_name = findViewById(R.id.contact_name);
@@ -127,6 +139,25 @@ public class WholesaleInspectionActivity extends AppCompatActivity {
         section_d_9 = findViewById(R.id.section_d_9);
         section_d_10 = findViewById(R.id.section_d_10);
         section_d_11 = findViewById(R.id.section_d_11);
+        section_e_1 = findViewById(R.id.section_e_1);
+        section_e_2 = findViewById(R.id.section_e_2);
+        section_e_3 = findViewById(R.id.section_e_3);
+        section_e_4 = findViewById(R.id.section_e_4);
+        section_e_5 = findViewById(R.id.section_e_5);
+        section_e_6 = findViewById(R.id.section_e_6);
+        section_e_7 = findViewById(R.id.section_e_7);
+        section_e_8 = findViewById(R.id.section_e_8);
+        section_e_9 = findViewById(R.id.section_e_9);
+        section_e_10 = findViewById(R.id.section_e_10);
+        section_e_11 = findViewById(R.id.section_e_11);
+        section_e_12 = findViewById(R.id.section_e_12);
+        section_f_1 = findViewById(R.id.section_f_1);
+        section_f_2 = findViewById(R.id.section_f_2);
+        section_f_3 = findViewById(R.id.section_f_3);
+        section_f_4 = findViewById(R.id.section_f_4);
+        section_f_5 = findViewById(R.id.section_f_5);
+        section_f_6 = findViewById(R.id.section_f_6);
+        section_f_7 = findViewById(R.id.section_f_7);
 
         section_a_1.setOnCheckedChangeListener((radioGroup, checked_id) -> {
             if(checked_id == R.id.section_a_1_no){
@@ -437,6 +468,196 @@ public class WholesaleInspectionActivity extends AppCompatActivity {
 
             calculateSectionD();
         });
+
+        section_e_1.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_e_1_no){
+                section_e_1_int = 0;
+            } else if(checked_id == R.id.section_e_1_yes){
+                section_e_1_int = 1;
+            }
+
+            calculateSectionE();
+        });
+
+        section_e_2.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_e_2_no){
+                section_e_2_int = 0;
+            } else if(checked_id == R.id.section_e_2_yes){
+                section_e_2_int = 1;
+            }
+
+            calculateSectionE();
+        });
+
+        section_e_3.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_e_3_no){
+                section_e_3_int = 0;
+            } else if(checked_id == R.id.section_e_3_yes){
+                section_e_3_int = 1;
+            }
+
+            calculateSectionE();
+        });
+
+        section_e_4.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_e_4_no){
+                section_e_4_int = 0;
+            } else if(checked_id == R.id.section_e_4_yes){
+                section_e_4_int = 1;
+            }
+
+            calculateSectionE();
+        });
+
+        section_e_5.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_e_5_no){
+                section_e_5_int = 0;
+            } else if(checked_id == R.id.section_e_5_yes){
+                section_e_5_int = 1;
+            }
+
+            calculateSectionE();
+        });
+
+        section_e_6.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_e_6_no){
+                section_e_6_int = 0;
+            } else if(checked_id == R.id.section_e_6_yes){
+                section_e_6_int = 1;
+            }
+
+            calculateSectionE();
+        });
+
+        section_e_7.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_e_7_no){
+                section_e_7_int = 0;
+            } else if(checked_id == R.id.section_e_7_yes){
+                section_e_7_int = 1;
+            }
+
+            calculateSectionE();
+        });
+
+        section_e_8.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_e_8_no){
+                section_e_8_int = 0;
+            } else if(checked_id == R.id.section_e_8_yes){
+                section_e_8_int = 1;
+            }
+
+            calculateSectionE();
+        });
+
+        section_e_9.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_e_9_no){
+                section_e_9_int = 0;
+            } else if(checked_id == R.id.section_e_9_yes){
+                section_e_9_int = 1;
+            }
+
+            calculateSectionE();
+        });
+
+        section_e_10.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_e_10_no){
+                section_e_10_int = 0;
+            } else if(checked_id == R.id.section_e_10_yes){
+                section_e_10_int = 1;
+            }
+
+            calculateSectionE();
+        });
+
+        section_e_11.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_e_11_no){
+                section_e_11_int = 0;
+            } else if(checked_id == R.id.section_e_11_yes){
+                section_e_11_int = 1;
+            }
+
+            calculateSectionE();
+        });
+
+        section_e_12.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_e_11_no){
+                section_e_11_int = 0;
+            } else if(checked_id == R.id.section_e_11_yes){
+                section_e_11_int = 1;
+            }
+
+            calculateSectionE();
+        });
+
+        section_f_1.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_f_1_no){
+                section_f_1_int = 0;
+            } else if(checked_id == R.id.section_f_1_yes){
+                section_f_1_int = 1;
+            }
+
+            calculateSectionF();
+        });
+
+        section_f_2.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_f_2_no){
+                section_f_2_int = 0;
+            } else if(checked_id == R.id.section_f_2_yes){
+                section_f_2_int = 1;
+            }
+
+            calculateSectionF();
+        });
+
+        section_f_3.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_f_3_no){
+                section_f_3_int = 0;
+            } else if(checked_id == R.id.section_f_3_yes){
+                section_f_3_int = 1;
+            }
+
+            calculateSectionF();
+        });
+
+        section_f_4.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_f_4_no){
+                section_f_4_int = 0;
+            } else if(checked_id == R.id.section_f_4_yes){
+                section_f_4_int = 1;
+            }
+
+            calculateSectionF();
+        });
+
+        section_f_5.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_f_5_no){
+                section_f_5_int = 0;
+            } else if(checked_id == R.id.section_f_5_yes){
+                section_f_5_int = 1;
+            }
+
+            calculateSectionF();
+        });
+
+        section_f_6.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_f_6_no){
+                section_f_6_int = 0;
+            } else if(checked_id == R.id.section_f_6_yes){
+                section_f_6_int = 1;
+            }
+
+            calculateSectionF();
+        });
+
+        section_f_7.setOnCheckedChangeListener((radioGroup, checked_id) -> {
+            if(checked_id == R.id.section_f_7_no){
+                section_f_7_int = 0;
+            } else if(checked_id == R.id.section_f_7_yes){
+                section_f_7_int = 1;
+            }
+
+            calculateSectionF();
+        });
     }
 
     @SuppressLint("SetTextI18n")
@@ -491,7 +712,7 @@ public class WholesaleInspectionActivity extends AppCompatActivity {
                 section_d_6_int + section_d_7_int + section_d_8_int +
                 section_d_9_int + section_d_10_int + section_d_11_int;
 
-        double percentage = ((double) section_d_total / 11) * 100;
+        double percentage = Math.round(((double) section_d_total / 11) * 100);
 
         section_d_percentage.setText(percentage + " %");
         section_d_total_score.setText(String.valueOf(section_d_total));
@@ -500,10 +721,41 @@ public class WholesaleInspectionActivity extends AppCompatActivity {
     }
 
     @SuppressLint("SetTextI18n")
-    public void calculateTotals(){
-        int overall_total = section_a_total + section_b_total + section_c_total + section_d_total;
+    public void calculateSectionE(){
+        // calculate the average score
+        section_e_total = section_e_1_int + section_e_2_int +
+                section_e_3_int + section_e_4_int + section_e_5_int +
+                section_e_6_int + section_e_7_int + section_e_8_int +
+                section_e_9_int + section_e_10_int + section_e_11_int + section_e_12_int;
 
-        double overall_percentage = ((double) overall_total / 31) * 100;
+        double percentage = Math.round(((double) section_e_total / 12) * 100);
+
+        section_e_percentage.setText(percentage + " %");
+        section_e_total_score.setText(String.valueOf(section_e_total));
+
+        calculateTotals();
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void calculateSectionF(){
+        // calculate the average score
+        section_f_total = section_f_1_int + section_f_2_int +
+                section_f_3_int + section_f_4_int + section_f_5_int +
+                section_f_6_int + section_f_7_int;
+
+        double percentage = Math.round(((double) section_f_total / 7) * 100);
+
+        section_f_percentage.setText(percentage + " %");
+        section_f_total_score.setText(String.valueOf(section_f_total));
+
+        calculateTotals();
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void calculateTotals(){
+        int overall_total = section_a_total + section_b_total + section_c_total + section_d_total + section_e_total + section_f_total;
+
+        double overall_percentage = Math.round(((double) overall_total / 50) * 100);
 
         overall_percentage_score.setText(overall_percentage + " %");
         overall_total_score.setText(String.valueOf(overall_total));
@@ -586,9 +838,9 @@ public class WholesaleInspectionActivity extends AppCompatActivity {
                 String section_a_checklist = section_a_1_int + "," + section_a_2_int + "," + section_a_3_int + "," + section_a_4_int + "," + section_a_5_int;
                 String section_b_checklist = section_b_1_int + "," + section_b_2_int + "," + section_b_3_int + "," + section_b_4_int + "," + section_b_5_int;
                 String section_c_checklist = section_c_1_int + "," + section_c_2_int + "," + section_c_3_int + "," + section_c_4_int + "," + section_c_5_int + "," + section_c_6_int + "," + section_c_7_int + "," + section_c_8_int + "," + section_c_9_int + "," + section_c_10_int;
-                String section_d_checklist = "";
-                String section_e_checklist = "";
-                String section_f_checklist = "";
+                String section_d_checklist = section_d_1_int + "," + section_d_2_int + "," + section_d_3_int + "," + section_d_4_int + "," + section_d_5_int + "," + section_d_6_int + "," + section_d_7_int + "," + section_d_8_int + "," + section_d_9_int + "," + section_d_10_int + "," + section_d_11_int;
+                String section_e_checklist = section_e_1_int + "," + section_e_2_int + "," + section_e_3_int + "," + section_e_4_int + "," + section_e_5_int + "," + section_e_6_int + "," + section_e_7_int + "," + section_e_8_int + "," + section_e_9_int + "," + section_e_10_int + "," + section_e_11_int + "," + section_e_12_int;
+                String section_f_checklist = section_f_1_int + "," + section_f_2_int + "," + section_f_3_int + "," + section_f_4_int + "," + section_f_5_int + "," + section_f_6_int + "," + section_f_7_int;
 
                 data.put("additional_notes", additional_notes.getText().toString());
                 data.put("pharmacy_name", pharmacy_name_string);
--- api/clean/post_supervision_checklist_wholesale.php
+++ api/clean/post_supervision_checklist_wholesale.php
@@ -11,10 +11,14 @@ $support_supervision_date = mysqli_real_escape_string($conn, $_POST['support_sup
 $location = mysqli_real_escape_string($conn, $_POST['location']);
 $section_a_checklist = mysqli_real_escape_string($conn, $_POST['section_a_checklist']);
 $section_b_checklist = mysqli_real_escape_string($conn, $_POST['section_b_checklist']);
+$section_c_checklist = mysqli_real_escape_string($conn, $_POST['section_c_checklist']);
+$section_d_checklist = mysqli_real_escape_string($conn, $_POST['section_d_checklist']);
+$section_e_checklist = mysqli_real_escape_string($conn, $_POST['section_e_checklist']);
+$section_f_checklist = mysqli_real_escape_string($conn, $_POST['section_f_checklist']);
 $submitted_by = mysqli_real_escape_string($conn, $_POST['submitted_by']);
 $timestamp = mysqli_real_escape_string($conn, $_POST['timestamp']);
 
-$sql = "INSERT INTO psu_supervision_checklist_wholesale (additional_notes, pharmacy_name, contact_name, contact, supervising_pharmacist, reg_number, support_supervision_date, location, section_a_checklist, section_b_checklist, submitted_by, timestamp) VALUES ('$additional_notes','$pharmacy_name','$contact_name','$contact','$supervising_pharmacist','$reg_number','$support_supervision_date','$location','$section_a_checklist','$section_b_checklist','$submitted_by','$timestamp')";
+$sql = "INSERT INTO psu_supervision_checklist_wholesale (additional_notes, pharmacy_name, contact_name, contact, supervising_pharmacist, reg_number, support_supervision_date, location, section_a_checklist, section_b_checklist, section_c_checklist, section_d_checklist, section_e_checklist, section_f_checklist, submitted_by, timestamp) VALUES ('$additional_notes','$pharmacy_name','$contact_name','$contact','$supervising_pharmacist','$reg_number','$support_supervision_date','$location','$section_a_checklist','$section_b_checklist','$section_c_checklist','$section_d_checklist','$section_e_checklist','$section_f_checklist','$submitted_by','$timestamp')";
 
 if ($conn->query($sql) === TRUE) {
     echo $conn->insert_id;
