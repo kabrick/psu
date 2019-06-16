ede312af7b6f9c7e40140dd2d87f023b5fc983d2
kabrick <kabuyedouglas53@gmail.com>
2019-06-16 15:19:14 +0300

    add main view for e-cpd resource to view and read resources

--- android/app/src/main/java/ug/or/psu/psudrugassessmenttool/globalactivities/EcpdFeedActivity.java
+++ android/app/src/main/java/ug/or/psu/psudrugassessmenttool/globalactivities/EcpdFeedActivity.java
@@ -3,16 +3,33 @@ package ug.or.psu.psudrugassessmenttool.globalactivities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.CardView;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.SearchView;
 
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import ug.or.psu.psudrugassessmenttool.R;
+import ug.or.psu.psudrugassessmenttool.adapters.EcpdFeedAdapter;
+import ug.or.psu.psudrugassessmenttool.helpers.HelperFunctions;
+import ug.or.psu.psudrugassessmenttool.models.EcpdFeed;
+import ug.or.psu.psudrugassessmenttool.network.VolleySingleton;
 
-public class EcpdFeedActivity extends AppCompatActivity {
+public class EcpdFeedActivity extends AppCompatActivity implements EcpdFeedAdapter.EcpdFeedAdapterListener {
 
-    CardView card_view;
+    private List<EcpdFeed> formList;
+    private EcpdFeedAdapter mAdapter;
+    HelperFunctions helperFunctions;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +42,77 @@ public class EcpdFeedActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayUseLogoEnabled(true);
 
-        card_view = findViewById(R.id.card_view);
+        RecyclerView recyclerView = findViewById(R.id.recycler_ecpd_feed);
+        formList = new ArrayList<>();
+        mAdapter = new EcpdFeedAdapter(formList, this);
 
-        card_view.setOnClickListener(view -> {
-            startActivity(new Intent(EcpdFeedActivity.this, EcpdViewQuestionsActivity.class));
+        helperFunctions = new HelperFunctions(this);
+
+        progressBar = findViewById(R.id.progressBarEcpdFeed);
+
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        SearchView searchView = findViewById(R.id.search_ecpd_feed);
+
+        searchView.setQueryHint("Search cpd's");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // filter recycler view when query submitted
+                mAdapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                // filter recycler view when text is changed
+                mAdapter.getFilter().filter(query);
+                return false;
+            }
         });
+
+        fetchForms();
+    }
+
+    private void fetchForms() {
+        String url = helperFunctions.getIpAddress() + "get_cpd_forms.php";
+
+        JsonArrayRequest request = new JsonArrayRequest(url,
+                response -> {
+
+                    //hide the progress bar
+                    progressBar.setVisibility(View.GONE);
+
+                    if (response == null) {
+                        helperFunctions.genericDialog("No CPDs were found");
+                        return;
+                    }
+
+                    List<EcpdFeed> items = new Gson().fromJson(response.toString(), new TypeToken<List<EcpdFeed>>() {
+                    }.getType());
+
+                    formList.clear();
+                    formList.addAll(items);
+
+                    // refreshing recycler view
+                    mAdapter.notifyDataSetChanged();
+                }, error -> {
+            // error in getting json, so recursive call till successful
+            fetchForms();
+        });
+
+        VolleySingleton.getInstance(this).addToRequestQueue(request);
+    }
+
+    @Override
+    public void onFormSelected(EcpdFeed form) {
+        Intent intent = new Intent(this, EcpdMainViewActivity.class);
+        intent.putExtra("id", form.getId());
+        startActivity(intent);
     }
 
     @Override
---
+++ android/app/src/main/java/ug/or/psu/psudrugassessmenttool/globalactivities/EcpdMainViewActivity.java
@@ -0,0 +1,141 @@
+package ug.or.psu.psudrugassessmenttool.globalactivities;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.text.format.DateUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONException;
+
+import java.util.Objects;
+
+import ug.or.psu.psudrugassessmenttool.R;
+import ug.or.psu.psudrugassessmenttool.helpers.HelperFunctions;
+import ug.or.psu.psudrugassessmenttool.helpers.PreferenceManager;
+import ug.or.psu.psudrugassessmenttool.network.VolleySingleton;
+
+public class EcpdMainViewActivity extends AppCompatActivity {
+
+    Button btn_take_test, btn_cancel, btn_read_resource;
+    CardView resource_text_cardview;
+    String ecpd_id, has_test, resource_type, resource_text, resource_url;
+    HelperFunctions helperFunctions;
+    PreferenceManager preferenceManager;
+    TextView ecpd_title, ecpd_description, ecpd_timestamp, resource_text_textview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ecpd_main_view);
+
+        // add icon to the action bar
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+        helperFunctions = new HelperFunctions(this);
+        preferenceManager = new PreferenceManager(this);
+
+        ecpd_title = findViewById(R.id.ecpd_title);
+        ecpd_description = findViewById(R.id.ecpd_description);
+        ecpd_timestamp = findViewById(R.id.ecpd_timestamp);
+        btn_read_resource = findViewById(R.id.btn_read_resource);
+        btn_cancel = findViewById(R.id.btn_cancel);
+        btn_take_test = findViewById(R.id.btn_take_test);
+        resource_text_cardview = findViewById(R.id.resource_text_cardview);
+        resource_text_textview = findViewById(R.id.resource_text_textview);
+
+        btn_read_resource.setOnClickListener(view -> {
+            if (resource_type.equals("1")){
+                // text
+                resource_text_textview.setText(resource_text);
+                resource_text_cardview.setVisibility(View.VISIBLE);
+                btn_read_resource.setVisibility(View.GONE);
+                btn_take_test.setVisibility(View.GONE);
+                btn_cancel.setVisibility(View.VISIBLE);
+            } else if (resource_type.equals("2")) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(helperFunctions.getIpAddress() + resource_url));
+                startActivity(intent);
+            }
+        });
+
+        btn_cancel.setOnClickListener(view -> {
+            resource_text_cardview.setVisibility(View.GONE);
+            btn_read_resource.setVisibility(View.VISIBLE);
+            btn_take_test.setVisibility(View.VISIBLE);
+            btn_cancel.setVisibility(View.GONE);
+        });
+
+        btn_take_test.setOnClickListener(view -> {
+            if (has_test.equals("1")){
+                //
+            } else {
+                Toast.makeText(this, "No test available yet", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Bundle extras = getIntent().getExtras();
+
+        if (extras != null) {
+            ecpd_id = extras.getString("id", "1");
+        }
+
+        getDetails();
+    }
+
+    public void getDetails(){
+
+        helperFunctions.genericProgressBar("Fetching E-CPD...");
+
+        String network_address = helperFunctions.getIpAddress() + "get_cpd_form.php?id=" + ecpd_id;
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, network_address, null,
+                response -> {
+
+                    helperFunctions.stopProgressBar();
+
+                    try {
+                        //covert timestamp to readable format
+                        CharSequence timeAgo = DateUtils.getRelativeTimeSpanString(
+                                Long.parseLong(response.getString("timestamp")),
+                                System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS);
+
+                        ecpd_title.setText(response.getString("title"));
+                        ecpd_description.setText(response.getString("description"));
+                        has_test = response.getString("has_test");
+                        resource_type = response.getString("resource_type");
+                        resource_text = response.getString("resource_text");
+                        resource_url = response.getString("resource_url");
+
+                        String info = "Submitted by " + response.getString("author") + " - " + timeAgo;
+                        ecpd_timestamp.setText(info);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }, error -> {
+            helperFunctions.stopProgressBar();
+            helperFunctions.genericDialog("Failed to get E-CPD form");
+        });
+
+        VolleySingleton.getInstance(this).addToRequestQueue(request);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+}
