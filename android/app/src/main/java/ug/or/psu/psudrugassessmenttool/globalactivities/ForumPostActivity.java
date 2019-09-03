1a592156ab2336e350b8f6cfd2a284dcfca2a342
kabrick <kabuyedouglas53@gmail.com>
2019-09-03 15:42:54 +0300

    post a forum topic to be discussed

--- android/app/src/main/java/ug/or/psu/psudrugassessmenttool/globalactivities/ForumPostActivity.java
+++ android/app/src/main/java/ug/or/psu/psudrugassessmenttool/globalactivities/ForumPostActivity.java
@@ -1,6 +1,15 @@
 package ug.or.psu.psudrugassessmenttool.globalactivities;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.provider.OpenableColumns;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -11,19 +20,50 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.TimeoutError;
+import com.android.volley.toolbox.Volley;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
 import ug.or.psu.psudrugassessmenttool.R;
+import ug.or.psu.psudrugassessmenttool.helpers.HelperFunctions;
+import ug.or.psu.psudrugassessmenttool.helpers.PreferenceManager;
+import ug.or.psu.psudrugassessmenttool.network.VolleyMultipartRequest;
 
 public class ForumPostActivity extends AppCompatActivity {
 
     ImageView picture_imageview, document_imageview, poll_imageview;
     TextView count_text, post_forum, cancel_forum;
     EditText forum_title;
+    HelperFunctions helperFunctions;
+    PreferenceManager preferenceManager;
+    private RequestQueue rQueue;
+    boolean is_picture_set = false;
+    boolean is_attachment_set = false;
+    private String fileExtension;
+    String filename;
+    String picture_name;
+    private Uri filePath;
+    Bitmap bitmap;
     boolean correct_characters = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forum_post);
+
+        helperFunctions = new HelperFunctions(this);
+        preferenceManager = new PreferenceManager(this);
 
         picture_imageview = findViewById(R.id.picture_imageview);
         picture_imageview.setOnClickListener(pictureListener);
@@ -47,23 +87,154 @@ public class ForumPostActivity extends AppCompatActivity {
     }
 
     private final View.OnClickListener pictureListener = v -> {
-        //
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(galleryIntent, 1);
     };
 
     private final View.OnClickListener documentListener = v -> {
-        //
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("*/*");
+        String[] mimetypes = {"application/vnd.openxmlformats-officedocument.wordprocessingml.document", "application/msword", "application/pdf", "application/vnd.ms-excel", "application/ms-powerpoint"};
+        intent.putExtra(Intent.EXTRA_MIME_TYPES, mimetypes);
+        startActivityForResult(Intent.createChooser(intent, "Select attachment"), 2);
     };
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_CANCELED) {
+            return;
+        }
+
+        // check if user has selected an image
+        if (requestCode == 1) {
+            if (data != null) {
+                Uri contentURI = data.getData();
+                try {
+
+                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), contentURI);
+
+                    Uri uri = getImageUri(this, bitmap);
+                    String path = getRealPathFromURI(this, uri);
+                    String filename = path.substring(path.lastIndexOf("/")+1);
+
+                    if (filename.indexOf(".") > 0) {
+                        picture_name = filename.substring(0, filename.lastIndexOf("."));
+                    } else {
+                        picture_name =  filename;
+                    }
+
+                    // set that the picture has been received
+                    is_picture_set = true;
+
+                    Toast.makeText(this, "Picture has been added successfully", Toast.LENGTH_LONG).show();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        // check if the user has selected an attachment
+        if (requestCode == 2 && data != null && data.getData() != null) {
+            filePath = data.getData();
+            is_attachment_set = true;
+
+            Toast.makeText(this, "Document has been added successfully", Toast.LENGTH_LONG).show();
+
+            String path = getRealPathFromURI(this, filePath);
+            filename = path.substring(path.lastIndexOf("/")+1);
+
+            // for the file extension
+            int i = filename.lastIndexOf('.');
+            if (i > 0) {
+                fileExtension = filename.substring(i+1);
+            }
+
+            // for the file name
+            if (filename.indexOf(".") > 0) {
+                filename = filename.substring(0, filename.lastIndexOf("."));
+            }
+        }
+    }
+
     private final View.OnClickListener pollListener = v -> {
-        //
+        Toast.makeText(this, "Feature not ready", Toast.LENGTH_SHORT).show();
     };
 
     private final View.OnClickListener postForumListener = v -> {
-        //
+        if (correct_characters){
+            helperFunctions.genericProgressBar("Posting your forum topic");
+            String upload_URL = helperFunctions.getIpAddress() + "post_forum_topic.php";
+
+            VolleyMultipartRequest volleyMultipartRequest = new VolleyMultipartRequest(Request.Method.POST, upload_URL,
+                    response -> {
+                        rQueue.getCache().clear();
+
+                        helperFunctions.stopProgressBar();
+
+                        if (response.equals("1")){
+                            AlertDialog.Builder alert = new AlertDialog.Builder(ForumPostActivity.this);
+                            alert.setMessage("Your discussion forum topic has been posted and will be reviewed later for approval").setPositiveButton("Okay", (dialogInterface, i) -> helperFunctions.getDefaultDashboard(preferenceManager.getMemberCategory())).show();
+                        } else {
+                            helperFunctions.genericDialog("Something went wrong. Please try again later");
+                        }
+                    },
+                    error -> {
+                        helperFunctions.stopProgressBar();
+
+                        if (error instanceof TimeoutError || error instanceof NetworkError) {
+                            helperFunctions.genericDialog("Something went wrong. Please make sure you are connected to a working internet connection.");
+                        } else {
+                            helperFunctions.genericDialog("Something went wrong. Please try again later");
+                        }
+                    }) {
+                @Override
+                protected Map<String, String> getParams(){
+                    Map<String, String> params = new HashMap<>();
+                    // add the psu_id
+                    params.put("title", forum_title.getText().toString());
+                    params.put("author_id", preferenceManager.getPsuId());
+                    params.put("timestamp", String.valueOf(System.currentTimeMillis()));
+                    return params;
+                }
+
+                /*
+                 *pass files using below method
+                 * */
+                @Override
+                protected Map<String, DataPart> getByteData() throws IOException {
+                    Map<String, DataPart> params = new HashMap<>();
+
+                    if (is_picture_set){
+                        params.put("picture_filename", new DataPart(picture_name + ".png", getFileDataFromDrawable(bitmap)));
+                    }
+
+                    if (is_attachment_set){
+                        params.put("document_filename", new DataPart(filename + "."  + fileExtension, getBytesFromFile(filePath)));
+                    }
+
+                    return params;
+                }
+            };
+
+            volleyMultipartRequest.setRetryPolicy(new DefaultRetryPolicy(
+                    0,
+                    DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                    DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+            rQueue = Volley.newRequestQueue(ForumPostActivity.this);
+            rQueue.add(volleyMultipartRequest);
+        } else {
+            helperFunctions.genericDialog("Please ensure that your topic title is correctly formatted");
+        }
     };
 
     private final View.OnClickListener cancelForumListener = v -> {
-        //
+        finish();
+        helperFunctions.getDefaultDashboard(preferenceManager.getMemberCategory());
     };
 
     private final TextWatcher mTextEditorWatcher = new TextWatcher() {
@@ -83,4 +254,74 @@ public class ForumPostActivity extends AppCompatActivity {
 
         public void afterTextChanged(Editable s) {}
     };
+
+    private Uri getImageUri(Context context, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(context.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    private String getRealPathFromURI(Context context, Uri uri) {
+        Cursor returnCursor = context.getContentResolver().query(uri, null, null, null, null);
+        /*
+         * Get the column indexes of the data in the Cursor,
+         *     * move to the first row in the Cursor, get the data,
+         *     * and display it.
+         * */
+        assert returnCursor != null;
+        int nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+        int sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE);
+        returnCursor.moveToFirst();
+        String name = (returnCursor.getString(nameIndex));
+        String size = (Long.toString(returnCursor.getLong(sizeIndex)));
+        File file = new File(context.getFilesDir(), name);
+        try {
+            InputStream inputStream = context.getContentResolver().openInputStream(uri);
+            FileOutputStream outputStream = new FileOutputStream(file);
+            int read = 0;
+            int maxBufferSize = 1 * 1024 * 1024;
+            assert inputStream != null;
+            int bytesAvailable = inputStream.available();
+
+            //int bufferSize = 1024;
+            int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+
+            final byte[] buffers = new byte[bufferSize];
+            while ((read = inputStream.read(buffers)) != -1) {
+                outputStream.write(buffers, 0, read);
+            }
+            inputStream.close();
+            outputStream.close();
+
+        } catch (Exception e) {
+            //
+        }
+        return file.getPath();
+    }
+
+    public byte[] getFileDataFromDrawable(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    public byte[] getBytesFromFile(Uri uri) throws IOException {
+        try (InputStream iStream = getContentResolver().openInputStream(uri)) {
+            assert iStream != null;
+
+            byte[] bytesResult;
+            try (ByteArrayOutputStream byteBuffer = new ByteArrayOutputStream()) {
+                int bufferSize = 1024;
+                byte[] buffer = new byte[bufferSize];
+                int len;
+                while ((len = iStream.read(buffer)) != -1) {
+                    byteBuffer.write(buffer, 0, len);
+                }
+                bytesResult = byteBuffer.toByteArray();
+            }
+
+            return bytesResult;
+        }
+    }
 }
---
+++ api/post_forum_topic.php
@@ -0,0 +1,35 @@
+<?php
+
+include 'dbconfig.php';
+
+$picturesOriginalImgName = $_FILES['picture_filename']['name'];
+$picturesTempName = $_FILES['picture_filename']['tmp_name'];
+$picturesFolder = "discussion_forum/pictures/";
+$picturesUrl = "discussion_forum/pictures/".$picturesOriginalImgName;
+$documentsOriginalImgName = $_FILES['document_filename']['name'];
+$documentsTempName = $_FILES['document_filename']['tmp_name'];
+$documentsFolder = "discussion_forum/documents/";
+$documentsUrl = "discussion_forum/documents/".$documentsOriginalImgName;
+$title = $_POST['title'];
+$author_id = $_POST['author_id'];
+$timestamp = $_POST['timestamp'];
+
+if(!move_uploaded_file($picturesTempName,$picturesFolder.$picturesOriginalImgName)){
+    $picturesUrl = NULL;
+    $picturesOriginalImgName = NULL;
+}
+
+if(!move_uploaded_file($documentsTempName,$documentsFolder.$documentsOriginalImgName)){
+    $documentsUrl = NULL;
+    $documentsOriginalImgName = NULL;
+}
+
+$sql = "INSERT INTO psu_forums (title, picture_url, picture_name, document_url, document_name, author, created_at) VALUES ('$title','$picturesUrl','$picturesOriginalImgName','$documentsUrl','$documentsOriginalImgName','$author_id','$timestamp')";
+
+if($conn->query($sql) === TRUE){
+    echo "1";
+}else{
+    echo "0";
+}
+
+?>
