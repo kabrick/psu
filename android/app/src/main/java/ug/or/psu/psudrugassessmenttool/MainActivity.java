82938d5c8b3ccd86b5563da0f9c5590b2412c662
kabrick <kabuyedouglas53@gmail.com>
2019-09-05 10:36:48 +0300

    change view of the main activity and prep for upload

--- android/app/src/main/java/ug/or/psu/psudrugassessmenttool/MainActivity.java
+++ android/app/src/main/java/ug/or/psu/psudrugassessmenttool/MainActivity.java
@@ -44,15 +44,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // add icon to the action bar
-        Objects.requireNonNull(getSupportActionBar()).setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
-
         prefManager = new PreferenceManager(this);
         helperFunctions = new HelperFunctions(this);
 
-        FirebaseMessaging.getInstance().subscribeToTopic("test")
+        FirebaseMessaging.getInstance().subscribeToTopic("all")
                 .addOnCompleteListener(task -> {
                     //
                 });
