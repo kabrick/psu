678e8596603dca14e0adbd3dce42882a8f88f0a7
kabrick <kabuyedouglas53@gmail.com>
2020-06-24 14:05:50 +0300

    update packages

--- api/convert_tweets_to_news.php
+++ api/convert_tweets_to_news.php
@@ -14,7 +14,7 @@ $sql = "INSERT INTO psu_news (title, text, source, author, timestamp, photo, rev
 
 if ($conn->query($sql) === TRUE) {
 	send_push_notification("PSU Notification", $title, 0);
-	send_email_global("PSU News Alert", "A news post has been added on the PSU app with title " . $title . ". Please visit the application to view it.");
+	//send_email_global("PSU News Alert", "A news post has been added on the PSU app with title " . $title . ". Please visit the application to view it.");
 	return "1";
 } else {
     return "0";
